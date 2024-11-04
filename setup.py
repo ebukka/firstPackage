50d1841eabeccdbdc9fa4cd9c8fea4e99a9c32b3
ebukka <emmie.dibia@gmail.com>
2024-11-04 19:51:41 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='EDSA example python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/ebukka/myModule',
+    author='Emmanuel Ebuka',
+    author_email='emmie.dibia@gmail.com'
+)
